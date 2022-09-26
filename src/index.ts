a7657f7cef42f68babe99908ca97eea4a5e50091
Ethan-Prieur1 <ethan.prieur@mths.ca>
2022-09-26 13:20:23 +0000

    Third Commit

--- src/index.ts
+++ src/index.ts
@@ -8,22 +8,22 @@
 import promptSync from 'prompt-sync'
 
 const randomNumber = Math.floor(Math.random() * 6 + 1)
-const counter = 0
-While(true)
-{
+let counter = 0
+while (true) {
   const prompt = promptSync()
   const userString = prompt('Guess My Random Number (1-6): ')
   const userNumber = parseInt(userString)
-  counter + 1
+  counter++
+
   if (userNumber === randomNumber) {
-    console.log(`You Got it right?! (${counter} Guess)`)
+    console.log(`You Got it right?! (Guesses: ${counter})`)
     break
   } else if (randomNumber < userNumber) {
-    console.log(`You Guessed TOO HIGH!!! You DUNCE!`)
+    console.log('You Guessed TOO HIGH!!! You DUNCE!')
   } else if (randomNumber > userNumber) {
-    console.log(`You Guessed TOO LOW!! You FOOL!`)
+    console.log('You Guessed TOO LOW!! You FOOL!')
   } else {
-    ;`Invalid Input, Redeem Yourself and Try Again`
+    console.log('Invalid Input, Redeem Yourself and Try Again')
   }
 }
-console.log(`\nDone`)
+console.log('\nDone.')
